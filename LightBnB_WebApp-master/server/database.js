667a10adb42277f5a5eca15fee249b81de55dd2d
Steven Ngov <gl.steven.ngov@gmail.com>
2022-01-20 23:43:40 +0000

    Implement addProperty function with SQL queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -134,7 +134,6 @@ const getAllProperties = function(options, limit = 10) {
     } else {
       queryString += `AND owner_id = $${queryParamsLength} `;
     }
-
   }
 
   if (options.minimum_price_per_night) {
@@ -174,8 +173,6 @@ const getAllProperties = function(options, limit = 10) {
   LIMIT $${queryParamsLength};
   `;
 
-  console.log(queryString, queryParams);
-
   return pool
     .query(queryString, queryParams)
     .then(result => {
@@ -185,26 +182,21 @@ const getAllProperties = function(options, limit = 10) {
 };
 exports.getAllProperties = getAllProperties;
 
-// SELECT properties.id, title, cost_per_night, start_date, AVG(property_reviews.rating) AS average_rating
-// FROM reservations
-// JOIN properties ON reservations.property_id = properties.id
-// JOIN property_reviews ON property_reviews.property_id = properties.id
-// WHERE reservations.guest_id = 1 AND reservations.end_date < NOW()::DATE
-// GROUP BY properties.id, reservations.id
-// ORDER BY reservations.start_date
-// LIMIT 10;
-
-
-
 /**
  * Add a property to the database
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  return pool
+    .query(`
+      INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
+      VALUES (${property.owner_id}, '${property.title}', '${property.description}', '${property.thumbnail_photo_url}', '${property.cover_photo_url}', ${property.cost_per_night}, ${property.parking_spaces}, ${property.number_of_bathrooms}, ${property.number_of_bedrooms}, '${property.country}', '${property.street}', '${property.city}', '${property.province}', '${property.post_code}')
+      RETURNING *
+      `)
+    .then(res => {
+      console.log(res.rows);
+    })
+    .catch(err => console.log(err.message));
 };
 exports.addProperty = addProperty;
